ac7dfabc5479fdc06c164f4352da8fa7e6aad604
nedmarafawi <70671308+nedmarafawi@users.noreply.github.com>
2022-07-25 00:29:01 -0400

    remove/add darker background color, logo change

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ const showMenu = (toggleId, navId) => {
 showMenu('nav-toggle', 'nav-menu');
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav-link');
 
 function linkAction() {
   /*Active link*/
@@ -24,34 +24,3 @@ function linkAction() {
   navMenu.classList.remove('show');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-  origin: 'top',
-  distance: '80px',
-  duration: 2000,
-  reset: true,
-});
-
-/*SCROLL HOME*/
-sr.reveal('.home__title', {});
-sr.reveal('.button', { delay: 200 });
-sr.reveal('.home__img', { delay: 400 });
-sr.reveal('.home__social-icon', { interval: 200 });
-
-/*SCROLL ABOUT*/
-sr.reveal('.about__img', {});
-sr.reveal('.about__subtitle', { delay: 400 });
-sr.reveal('.about__text', { delay: 400 });
-
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle', {});
-sr.reveal('.skills__text', {});
-sr.reveal('.skills__data', { interval: 200 });
-sr.reveal('.skills__img', { delay: 600 });
-
-/*SCROLL WORK*/
-sr.reveal('.work__img', { interval: 200 });
-
-/*SCROLL CONTACT*/
-sr.reveal('.contact__input', { interval: 200 });
